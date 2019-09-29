02cc8dea0103fea33859fa6cf0843d686035bed2
QuangDuong120198 <tenminhladuong@gmail.com>
2019-09-29 22:44:22 +0700

    Update prettier rule, re-format, install necessary packages

--- pages/api/member/[id].js
+++ pages/api/member/[id].js
@@ -1,30 +1,4 @@
-const member = [
-    {
-        id: '17020191',
-        name: 'Ngo Quang Duong'
-    },
-    {
-        id: '17020772',
-        name: 'Nguyen Xuan Hoang'
-    },
-    {
-        id: '17021017',
-        name: 'Nguyen Tien Thanh'
-    },
-    {
-        id: '17020731',
-        name: 'Dinh The Hiep'
-    },
-    {
-        id: '17020853',
-        name: 'Le Thi Thuy Linh'
-    },
-    {
-        id: '16021213',
-        name: 'Nguyen Anh Tuan'
-    }
-];
-
+import { member } from '../../../mock-data';
 /**
  *
  * @param {import('next').NextApiRequest} req
@@ -32,10 +6,10 @@ const member = [
  * @returns {void}
  */
 export default (req, res) => {
-    const result = member.find(currentMember => currentMember.id === req.query.id);
-    if (result) {
-        res.json(result);
-        return;
-    }
-    res.end('Member Not Found');
+  const result = member.find(currentMember => currentMember.id === req.query.id);
+  if (result) {
+    res.json(result);
+    return;
+  }
+  res.end('Member Not Found');
 };
--- pages/api/member/index.js
+++ pages/api/member/index.js
@@ -1,3 +1,5 @@
+import { member } from '../../../mock-data';
+
 /**
  *
  * @param {import('next').NextApiRequest} req
@@ -5,30 +7,5 @@
  * @returns {void}
  */
 export default (req, res) => {
-    res.json([
-        {
-            id: '17020191',
-            name: 'Ngo Quang Duong'
-        },
-        {
-            id: '17020772',
-            name: 'Nguyen Xuan Hoang'
-        },
-        {
-            id: '17021017',
-            name: 'Nguyen Tien Thanh'
-        },
-        {
-            id: '17020731',
-            name: 'Dinh The Hiep'
-        },
-        {
-            id: '17020853',
-            name: 'Le Thi Thuy Linh'
-        },
-        {
-            id: '16021213',
-            name: 'Nguyen Anh Tuan'
-        }
-    ]);
+  res.json(member);
 };
