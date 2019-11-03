3fe824a9e164a41745e4ebde4b1bff309fb5f5d6
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-11-03 18:40:58 +0700

    Install react-select, tweaking Tuan's UI

--- pages/create-course/index.js
+++ pages/create-course/index.js
@@ -1,37 +1,26 @@
 import Head from 'next/head';
 import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/core/styles';
-import { deepOrange } from '@material-ui/core/colors';
 
-import Radio from '@material-ui/core/Radio';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from '@material-ui/core/Button';
+import NoSsr from '@material-ui/core/NoSsr';
 
-const OrangeRadio = withStyles({
-  root: {
-    color: '#FF5500',
-    '&$checked': {
-      color: '#FF5500'
-    }
-  },
-  checked: {}
-})(props => <Radio color="default" {...props} />);
+import ReactSelect from 'react-select';
+
+import withLayout from '../../components/lib/withLayout';
 
 const useStyles = makeStyles(theme => ({
   button: {
-    margin: theme.spacing(1),
-    backgroundColor: '#FF5500',
-    color: 'white',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
     fontWeight: 'bold',
     width: '15%',
     '&:hover': {
-      backgroundColor: deepOrange[900]
+      backgroundColor: theme.palette.primary.dark
     }
   },
   item: {
@@ -41,12 +30,10 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
     '&.Mui-focused fieldset': {
       borderWidth: '3px',
-      borderColor: 'black'
+      borderColor: theme.palette.common.black
     }
   }
 }));
-
-import withLayout from '../../components/lib/withLayout';
 
 function CreateCourse() {
   const classes = useStyles();
@@ -57,43 +44,38 @@ function CreateCourse() {
         <title>Create course</title>
       </Head>
       <Box>
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Grid container direction="column" spacing={1} alignItems="center" justify="center">
-            <Grid item xs={8} className={classes.item}>
+            <Grid item xs={12} sm={8} className={classes.item}>
               <Box className="pt-3" component="h1">
                 Create new course
               </Box>
               <Divider />
             </Grid>
-            <Grid item xs={8} className={classes.item}>
+            <Grid item xs={12} sm={8} className={classes.item}>
               <Box component="h3">Course Name</Box>
-              <TextField
-                className={classes.textField}
-                required
-                id="course-name"
-                variant="outlined"
-                label="Course name"
-                margin="normal"
-              />
+              <TextField className={classes.textField} required id="course-name" variant="outlined" />
             </Grid>
-            <Grid item xs={8} className={classes.item}>
+            <Grid item xs={12} sm={8} className={classes.item}>
               <Box component="h3">Description(Optional)</Box>
-              <TextField
-                className={classes.textField}
-                multiline
-                id="description"
-                variant="outlined"
-                label="Course description"
-                margin="normal"
-              />
+              <TextField className={classes.textField} multiline id="description" variant="outlined" />
             </Grid>
-            <Grid item xs={8} className={classes.item}>
+            {/* <Grid item xs={12} sm={8} className={classes.item}>
               <RadioGroup defaultValue="Public" aria-label="privacy" name="customized-radios" row>
-                <FormControlLabel value="public" control={<OrangeRadio />} label="Public" />
-                <FormControlLabel value="private" control={<OrangeRadio />} label="Private" />
+                <FormControlLabel value="public" control={<Radio color="primary" />} label="Public" />
+                <FormControlLabel value="private" control={<Radio color="primary" />} label="Private" />
               </RadioGroup>
+            </Grid> */}
+            <Grid item xs={12} sm={8} className={classes.item}>
+              <Box component="h3">Topics</Box>
+              <NoSsr>
+                <ReactSelect isMulti options={[{ label: 'A', value: 0 }, { label: 'B', value: 1 }]} />
+              </NoSsr>
             </Grid>
-            <Grid item xs={8} className={classes.item}>
+            <Grid item xs={12} sm={8} className={classes.item}>
+              <Divider />
+            </Grid>
+            <Grid item xs={12} sm={8} className={classes.item}>
               <Button variant="contained" className={classes.button}>
                 Create
               </Button>
