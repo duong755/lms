1634148e5bf20d518d365dc0abdc425650ccbd5d
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-11-04 23:57:45 +0700

    Add ESlint rules, install material-ui/pickers, add MuiPickerUtilsProvider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import App from 'next/app';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DayjsUtils from '@date-io/dayjs';
 import { CookiesProvider } from 'react-cookie';
 
 import CustomThemeProvider from '../components/theme/CustomThemeProvider';
@@ -18,8 +19,10 @@ class CustomApp extends App {
       <>
         <CookiesProvider>
           <CustomThemeProvider theme={pageProps.theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
+            <MuiPickersUtilsProvider utils={DayjsUtils}>
+              <CssBaseline />
+              <Component {...pageProps} />
+            </MuiPickersUtilsProvider>
           </CustomThemeProvider>
         </CookiesProvider>
       </>
