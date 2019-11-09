5e3da6f5c4bba96b7edc672dd95e90a887ad8e7d
NATuan <anhtuan.051998@gmail.com>
2019-11-10 00:43:09 +0700

    course detail screen lesson

---
+++ pages/course/[courseId]/index.js
@@ -0,0 +1,162 @@
+import Head from 'next/head';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+import withLayout from '../../../components/lib/withLayout';
+
+const useStyle = makeStyles((theme) => ({
+  breadCrumbs: {
+    fontSize: 'x-large',
+    fontWeight: 'lighter'
+  },
+  breadCrumbsCur: {
+    fontSize: 'x-large',
+    fontWeight: theme.typography.fontWeightBold
+  },
+  button: {
+    color: theme.palette.primary.main,
+    opacity: '0.5'
+  },
+  currentButton: {
+    color: theme.palette.primary.main,
+    fontWeight: theme.typography.fontWeightBold
+  },
+  container: {
+    margin: theme.spacing(2, 0)
+  },
+  item: {
+    marginRight: theme.spacing(2)
+  },
+  lesson: {
+    margin: theme.spacing(1, 0),
+    padding: theme.spacing(2)
+  },
+  title: {
+    fontWeight: theme.typography.fontWeightBold,
+    color: theme.palette.primary.main,
+    fontSize: theme.typography.h6.fontSize
+  },
+  navButton: {
+    padding: theme.spacing(1)
+  }
+}));
+
+function CourseDetail() {
+  const classes = useStyle();
+
+  return (
+    <>
+      <Head>
+        <title>Course detail</title>
+      </Head>
+      <Box p={5}>
+        <Container maxWidth="xl">
+          <Grid container direction="column" className={classes.container}>
+            <Grid item xs={12}>
+              <Breadcrumbs className={classes.breadCrumbs} aria-label="breadcrumb">
+                <Link color="inherit" href="/">
+                  Username
+                </Link>
+                <Typography className={classes.breadCrumbsCur} color="textPrimary">
+                  CourseName
+                </Typography>
+              </Breadcrumbs>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>
+                <Icon>calendar_today</Icon>Created on abcxyz
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container justify="flex-start" className={classes.container}>
+            <Grid item xs={2}>
+              <Button variant="outlined" className={classes.currentButton}>
+                Lesson
+              </Button>
+            </Grid>
+            <Grid item xs={2} className={classes.item}>
+              <Button className={classes.button}>Exercise</Button>
+            </Grid>
+            <Grid item xs={2} className={classes.item}>
+              <Button className={classes.button}>Exam</Button>
+            </Grid>
+            <Grid item xs={2} className={classes.item}>
+              <Button className={classes.button}>Students</Button>
+            </Grid>
+            <Grid item xs={2} className={classes.item}>
+              <Button className={classes.button}>Join request</Button>
+            </Grid>
+          </Grid>
+          <Grid container justify="flex-end" className={classes.container}>
+            <Button variant="contained" color="primary">
+              <Icon>add</Icon>Create Lesson
+            </Button>
+          </Grid>
+          <Grid container justify="center" direction="column" className={classes.container}>
+            <Grid item>
+              <Paper className={classes.lesson}>
+                <Typography className={classes.title}>Lesson title</Typography>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper className={classes.lesson}>
+                <Typography className={classes.title}>Lesson title</Typography>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper className={classes.lesson}>
+                <Typography className={classes.title}>Lesson title</Typography>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container justify="center">
+            <Grid item>
+              <ButtonGroup variant="outlined" color="primary" size="large">
+                <Button className={classes.navButton}>
+                  <Icon>keyboard_arrow_left</Icon>
+                </Button>
+                <Button variant="contained" className={classes.navButton}>
+                  1
+                </Button>
+                <Button className={classes.navButton}>2</Button>
+                <Button className={classes.navButton}>3</Button>
+                <Button className={classes.navButton}>...</Button>
+                <Button className={classes.navButton}>10</Button>
+                <Button className={classes.navButton}>
+                  <Icon>keyboard_arrow_right</Icon>
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+export default withLayout(CourseDetail);
