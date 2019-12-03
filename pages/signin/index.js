db456f5a90a11f99b2cb51d860fc72a420a1fff7
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-12-03 15:23:24 +0700

    Upgrade formik, validate sign in form in client side

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
-import axios from 'axios';
+import clsx from 'clsx';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
@@ -9,10 +8,16 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 import Link from '@material-ui/core/Link';
-import Hidden from '@material-ui/core/Hidden';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+import { UncontrolledAlert } from 'reactstrap';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import withLayout from '../../components/lib/withLayout';
 
@@ -30,31 +35,101 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     paddingLeft: theme.spacing(3),
     paddingRight: theme.spacing(3)
+  },
+  formHelperText: {
+    color: theme.palette.error.main
   }
 }));
 
-function Signin() {
+const signInValidationSchema = Yup.object().shape({
+  usernameOrEmail: Yup.string().required('Username or Email is required'),
+  password: Yup.string().required('Password is required'),
+  error: Yup.boolean().notRequired()
+});
+
+const initialSignInFormValue = {
+  usernameOrEmail: '',
+  password: '',
+  error: false
+};
+
+function SignInForm() {
   const classes = useStyles();
-  const [values, setValues] = useState({});
+  const formik = useFormik({
+    initialValues: initialSignInFormValue,
+    validationSchema: signInValidationSchema
+  });
 
-  function handleChange(event) {
-    event.persist();
-    setValues((values) => ({
-      ...values,
-      [event.target.name]: event.target.value
-    }));
-  }
+  const { values, handleChange, handleSubmit, touched, errors, handleReset } = formik;
 
-  function handleSubmit(event) {
-    if (event) {
-      event.preventDefault();
-    }
-    console.log(values);
-    axios
-      .post('/signin', values)
-      .then((res) => console.log('Success ', res))
-      .catch((err) => console.error('Error ', err));
-  }
+  return (
+    <form onSubmit={handleSubmit} onReset={handleReset}>
+      {values.error && <UncontrolledAlert color="danger">Wrong username/email or password</UncontrolledAlert>}
+      <Paper>
+        <Box pt={2} px={2}>
+          <TextField
+            variant="outlined"
+            type="text"
+            label="Username or Email"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Icon>account_box</Icon>
+                </InputAdornment>
+              )
+            }}
+            value={values.usernameOrEmail}
+            onChange={handleChange('usernameOrEmail')}
+            helperText={touched.usernameOrEmail && errors.usernameOrEmail}
+            FormHelperTextProps={{
+              className: clsx(classes.formHelperText)
+            }}
+          />
+        </Box>
+        <Box pt={2} px={2}>
+          <TextField
+            variant="outlined"
+            type="password"
+            label="Password"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Icon>lock</Icon>
+                </InputAdornment>
+              )
+            }}
+            value={values.password}
+            onChange={handleChange('password')}
+            helperText={touched.password && errors.password}
+            FormHelperTextProps={{
+              className: clsx(classes.formHelperText)
+            }}
+          />
+        </Box>
+        <Box p={2}>
+          <Button fullWidth variant="contained" type="submit" color="primary" onClick={handleSubmit}>
+            Sign in
+          </Button>
+        </Box>
+        <Box pb={2} px={2}>
+          <Button fullWidth variant="text">
+            <NextLink href="/">
+              <Link color="primary" href="/">
+                Forgot your password
+              </Link>
+            </NextLink>
+          </Button>
+        </Box>
+      </Paper>
+    </form>
+  );
+}
+
+function SignIn() {
+  const classes = useStyles();
+
   return (
     <>
       <Head>
@@ -62,55 +137,15 @@ function Signin() {
       </Head>
       <Box>
         <Container maxWidth="xl">
-          <Grid className={classes.root} container component="main" alignItems="center">
-            <Grid item xs={12} sm={6}>
+          <Grid className={classes.root} container component="main" justify="center" alignItems="center">
+            <Grid item xs={12} sm={8} md={6}>
               <Box className={classes.form} display="flex" flexDirection="column" alignItems="stretch">
                 <Typography gutterBottom align="center" variant="h4">
                   <strong>Sign in to OpenLMS</strong>
                 </Typography>
-                <Paper>
-                  <form onSubmit={handleSubmit}>
-                    <Box paddingTop={3} paddingX={3}>
-                      <Box paddingBottom={1} fontWeight="bold">
-                        Username
-                      </Box>
-                      <TextField
-                        fullWidth
-                        required
-                        autoFocus
-                        variant="outlined"
-                        onChange={handleChange}
-                        name="account"
-                        type="email"
-                      />
-                    </Box>
-                    <Box paddingLeft={3} paddingTop={3} paddingRight={3}>
-                      <Box paddingBottom={1} fontWeight="bold">
-                        Password
-                      </Box>
-                      <TextField
-                        fullWidth
-                        required
-                        variant="outlined"
-                        onChange={handleChange}
-                        name="password"
-                        type="password"
-                      />
-                    </Box>
-                    <Box paddingLeft={3} paddingTop={1.5} paddingRight={3}>
-                      <Button fullWidth variant="contained" type="submit" color="primary">
-                        Sign in
-                      </Button>
-                    </Box>
-                    <Box paddingX={3} paddingY={1.5}>
-                      <Button fullWidth variant="text">
-                        <NextLink href="/">
-                          <Link href="/">forgot your password</Link>
-                        </NextLink>
-                      </Button>
-                    </Box>
-                  </form>
-                </Paper>
+
+                <SignInForm />
+
                 <Box height={30} />
                 <Button color="primary" variant="outlined" fullWidth>
                   <NextLink href="/signup">
@@ -119,14 +154,6 @@ function Signin() {
                 </Button>
               </Box>
             </Grid>
-            <Grid item xs={false} sm={6}>
-              <Hidden smDown>
-                <img
-                  className={classes.image}
-                  src="https://lh3.googleusercontent.com/UGQxZMBLsql2ED-_Kx7GHy0c7l3O64uhqeubwu1A-53VIamRM_VsDth_EyKEXnrwIwxUetQmY_3c03gHCMqAG49UJi7Fom1kqwNVdR9Az13WIrgGBBbTKxIZlKy8xdOuXuKJcPeE-1nLQ2qsBzReTYupwLqXr7ie2sxjPa8jafUEK4L7zlyLfjdlAfoz6GzDgoYva2sqJALVYx4Uv_UUemEZRZWmEQGPImpJfVQxamw0xPSuak39lHflrVDQUhzZAkw5Mf1fMUuKMBkD3sb9T8UdnYMRhDp8XUM6ZLBhVfIWASw_pSuPSVHTSWvVrGobxf0pp4BmgjZ6l39lK14OtkHcQO3_R4a9DdO3D7uRk7ad85mWBpGO8EkouV0xCi5MsXFTOuF5vd89SnpZFmLvMW6_TEXYKHf4RpIECUS_8I_MrLEW2aPmY9ir2DkIlnzJUY59g754P9O6AtCyik3FHFWxRYJzfQKnCQUk7JaTMWK15BrW3CW_ka1Ah_rAgmTAjsMXYqMWqnDPjIJx3HQOZWBYSYCIRrHk9hRfOuPlY4M2umzZPKSGKOOKc4mhBFr0P9F5BvD_4xH4b2DlSwZCxDtwbAQtiYcSrloLdCU5caHVWM4IcZSss5jlYDI=w1366-h590"
-                />
-              </Hidden>
-            </Grid>
           </Grid>
         </Container>
       </Box>
@@ -134,4 +161,4 @@ function Signin() {
   );
 }
 
-export default withLayout(Signin);
+export default withLayout(SignIn);
