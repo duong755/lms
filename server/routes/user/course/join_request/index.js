d94123f7132435d80a293629d4095432ef5452ac
nxuanhoang <xuanhoang30071999@gmail.com>
2019-12-05 04:55:54 +0700

    api get join_request with pagination

--- server/routes/user/course/join_request/index.js
+++ server/routes/user/course/join_request/index.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 
+const joinRequestService = require('../../../../services/JoinRequest');
 const joinRequestRouter = Router({ mergeParams: true });
 
 /**
  * join_request pagination
  */
-joinRequestRouter.get('/', (req, res) => {
-  res.end('/api/user/:userId/course/:courseId/join_request');
+joinRequestRouter.get('/', async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const result = await joinRequestService.getJoinRequests(req.params.userId, req.params.courseId, page);
+    const joinRequests = result.body.hits.hits.map((current) => current._source);
+    res.status(200).json({ joinRequests: joinRequests, total: result.body.hits.total });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
 
 /**
