06910d3ef49074525820d7147b93f9061c4412a0
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-12-14 23:32:59 +0700

    Course pagination at profile page

--- pages/user/[userId]/index.js
+++ pages/user/[userId]/index.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+/**
+ * @typedef {'teacher' | 'student'} UserType
+ * @typedef {{ id: string, email: string, username: string, type: UserType, info: Object<string, string> }} User
+ * @typedef {{ id: string, teacher_id: string, course_name: string, created_at: string, archive?: boolean, description?: string, topics?: string | string[], members?: string | string[] }} Course
+ * @typedef {{ currentUser?: User | null, total?: number | null, courses?: Course[] }} ProfilePageProps
+ */
+import { useState, useCallback, useEffect } from 'react';
 import Head from 'next/head';
+import NextLink from 'next/link';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import fetch from 'isomorphic-unfetch';
 import clsx from 'clsx';
+import dayjs from 'dayjs';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
@@ -13,17 +21,27 @@ import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 
 import withLayout from '../../../components/lib/withLayout';
 import absURL from '../../../components/helpers/URL';
 
 const useStyles = makeStyles((theme) => ({
   courseContainer: {
-    padding: theme.spacing(3, 2),
-    margin: theme.spacing(2, 0)
+    padding: theme.spacing(2),
+    margin: theme.spacing(1, 0)
   },
   courseDescription: {
-    padding: theme.spacing(2, 0)
+    padding: theme.spacing(1, 0)
   },
   courseTopic: {
     marginRight: theme.spacing(2)
@@ -53,18 +71,109 @@ const useStyles = makeStyles((theme) => ({
   otherInfo: {
     display: 'flex',
     flexDirection: 'column'
+  },
+  cell: {
+    padding: theme.spacing(2, 0)
   }
 }));
 
-function ProfilePage(props) {
-  const { currentUser, total = 0 } = props;
-  const [currentTab, setCurrentTab] = useState(0);
+/**
+ * @type {React.FunctionComponent<{ course: Course }>}
+ */
+const CourseItem = (props) => {
+  const { course } = props;
+  const { id, teacher_id, course_name, created_at, description, archive, topics, members } = course;
   const classes = useStyles();
+
+  const allTopics = topics ? ((topics instanceof Array ? topics : [topics])) : [];
+
+  const courseMembers = useCallback((members) => {
+    if (members) {
+      if (members instanceof Array) {
+        return members.length;
+      }
+      return 1;
+    }
+    return 0;
+  }, []);
+
+  return ((
+    <Paper className={clsx(classes.courseContainer)}>
+      <Typography variant="h5">
+        <NextLink href="/user/[userId]/course/[courseId]" as={`/user/${teacher_id}/course/${id}`} prefetch={false}>
+          <Link href={`/user/${teacher_id}/course/${id}`} color="primary">
+            <strong>{course_name}</strong>
+          </Link>
+        </NextLink>
+        {archive && (
+          <Button size="small" variant="outlined">
+            Archived
+          </Button>
+        )}
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Box display="flex" alignItems="center">
+            <Icon>access_time</Icon>&nbsp;
+            <Typography variant="caption">{dayjs(created_at).format('YYYY MMM d, hh:mm A')}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Box display="flex" alignItems="center">
+            <Icon>group</Icon>&nbsp;
+            <Typography variant="caption">{courseMembers(members)} members</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box py={2}>
+        <Typography>{description}</Typography>
+      </Box>
+      <Box>
+        {allTopics.map((currentTopic) => (
+          <Box component="span" key={currentTopic} mr={1}>
+            <Button variant="contained" color="primary" size="small">
+              {currentTopic}
+            </Button>
+          </Box>
+        ))}
+      </Box>
+    </Paper>
+  ));
+};
+
+/**
+ * @type {React.FunctionComponent<ProfilePageProps>}
+ */
+const ProfilePage = (props) => {
+  const { currentUser } = props;
+  const [currentTab, setCurrentTab] = useState(0);
+  const [totalCourses, setTotalCourses] = useState(Number(props.total));
+  const [courses, setCourses] = useState(props.courses);
+  const [currentPage, setCurrentPage] = useState(1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    const urlPath = `/api/user/${currentUser.id}/course?page=${currentPage}`;
+    (async () => {
+      try {
+        const coursesRes = await fetch(absURL(urlPath), {
+          method: 'GET',
+          credentials: 'include',
+          mode: 'same-origin'
+        });
+        const json = await coursesRes.json();
+        setCourses(json.courses);
+        setTotalCourses(json.total);
+      } catch (courseErr) {
+        console.error(courseErr);
+      }
+    })();
+  }, [currentPage]);
 
   const otherInfo =
     currentUser && _.toPairs(currentUser.info).filter((currentPair) => currentPair[0] !== 'image' && currentPair[1]);
 
-  return (
+  return ((
     <>
       <Head>
         <title>{currentUser ? `${currentUser.username}'s profile` : 'User Not Found'}</title>
@@ -100,8 +209,36 @@ function ProfilePage(props) {
                   setCurrentTab(value);
                 }}
               >
-                <Tab value={0} label={`Courses (${total})`} />
+                <Tab value={0} label={`Courses (${totalCourses})`} />
               </Tabs>
+              {currentTab === 0 && (
+                <Table>
+                  <TableBody>
+                    {courses.map((currentCourse) => {
+                      return (
+                        <TableRow key={currentCourse.id}>
+                          <TableCell className={clsx(classes.cell)}>
+                            <CourseItem course={currentCourse} />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow>
+                      <TablePagination
+                        count={totalCourses}
+                        page={currentPage - 1}
+                        onChangePage={(event, page) => {
+                          setCurrentPage(page + 1);
+                        }}
+                        rowsPerPage={10}
+                        rowsPerPageOptions={[10]}
+                      />
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              )}
             </>
           ) : (
             <Box display="flex" flexDirection="column" alignItems="center" p={5}>
@@ -117,8 +254,8 @@ function ProfilePage(props) {
         </Container>
       </Box>
     </>
-  );
-}
+  ));
+};
 
 ProfilePage.getInitialProps = async (context) => {
   const { userId } = context.query;
@@ -140,6 +277,17 @@ ProfilePage.getInitialProps = async (context) => {
   };
 };
 
+const CoursePropType = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  course_name: PropTypes.string.isRequired,
+  created_at: PropTypes.string.isRequired,
+  members: PropTypes.arrayOf(PropTypes.string)
+});
+
+CourseItem.propTypes = {
+  course: CoursePropType
+};
+
 ProfilePage.propTypes = {
   currentUser: PropTypes.shape({
     id: PropTypes.string.isRequired,
@@ -152,14 +300,7 @@ ProfilePage.propTypes = {
       image: PropTypes.string
     })
   }),
-  courses: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      course_name: PropTypes.string.isRequired,
-      created_at: PropTypes.string.isRequired,
-      members: PropTypes.arrayOf(PropTypes.string)
-    })
-  ),
+  courses: PropTypes.arrayOf(CoursePropType),
   total: PropTypes.number
 };
 
