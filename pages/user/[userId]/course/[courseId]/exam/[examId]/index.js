1325c12d216b5a34aaec4265e4c1b6a95dbc2a7f
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-12-14 23:59:17 +0700

    Delete edit pages

---
+++ pages/user/[userId]/course/[courseId]/exam/[examId]/index.js
@@ -0,0 +1,7 @@
+import withLayout from '../../../../../../../components/lib/withLayout';
+
+const ExamPage = () => {
+  return <div></div>;
+};
+
+export default withLayout(ExamPage);
---
+++ pages/user/[userId]/course/[courseId]/exam/create.js
@@ -0,0 +1,7 @@
+import withLayout from '../../../../../../components/lib/withLayout';
+
+const CreateExamPage = () => {
+  return <div></div>;
+};
+
+export default withLayout(CreateExamPage);
